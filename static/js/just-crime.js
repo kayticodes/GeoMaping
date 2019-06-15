24c70bccf522a8aad4d1c7234debd6854204d941
kaytipotgieter <47190540+kaytipotgieter@users.noreply.github.com>
2019-06-15 14:23:51 -0700

    Add files via upload

---
+++ static/js/just-crime.js
@@ -0,0 +1,73 @@
+var myMap = L.map("map", {
+  center: [37.7749, -122.4194],
+  zoom: 13
+});
+
+// Adding tile layer
+L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+  attribution: "Map data &copy; <a href='https://www.openstreetmap.org/'>OpenStreetMap</a> contributors, <a href='https://creativecommons.org/licenses/by-sa/2.0/'>CC-BY-SA</a>, Imagery © <a href='https://www.mapbox.com/'>Mapbox</a>",
+  maxZoom: 18,
+  id: "mapbox.streets",
+  accessToken: API_KEY
+}).addTo(myMap);
+
+var newtry = "http://data-lakecountyil.opendata.arcgis.com/datasets/3e0c1eb04e5c48b3be9040b0589d3ccf_8.geojson";
+
+// d3.json(newtry, function(response) {
+
+//   console.log("hi", response);
+
+//   L.geoJSON(response).addTo(myMap);
+  // for (var i = 0; i < response.length; i++) {
+  //   var location = response.features;
+
+  //   console.log("hi again", location);
+  //   if (location) {
+  //     L.marker([location[1], location]).addTo(myMap);
+  //   }
+  // }
+var chooseColor= "yellow";
+  // Grabbing our GeoJSON data..
+d3.json(newtry, function(data) {
+  // Creating a geoJSON layer with the retrieved data
+  L.geoJson(data, {
+    // Style each feature (in this case a neighborhood)
+    style: function(feature) {
+      return {
+        color: "white",
+        fillColor: "yellow",
+        fillOpacity: 0.5,
+        weight: 1.5
+      };
+    },
+    // Called on each feature
+    onEachFeature: function(feature, layer) {
+      // Set mouse events to change map styling
+      layer.on({
+        // When a user's mouse touches a map feature, the mouseover event calls this function, that feature's opacity changes to 90% so that it stands out
+        mouseover: function(event) {
+          layer = event.target;
+          layer.setStyle({
+            fillOpacity: 0.9
+          });
+        },
+        // When the cursor no longer hovers over a map feature - when the mouseout event occurs - the feature's opacity reverts back to 50%
+        mouseout: function(event) {
+          layer = event.target;
+          layer.setStyle({
+            fillOpacity: 0.5
+          });
+        },
+        // When a feature (neighborhood) is clicked, it is enlarged to fit the screen
+        click: function(event) {
+          map.fitBounds(event.target.getBounds());
+        }
+      });
+      // Giving each feature a pop-up with information pertinent to it
+      layer.bindPopup("<h1>" + feature.properties.NAME + "</h1> <hr> <h2>" + feature.properties.Obesity + "</h2>");
+
+    }
+  }).addTo(map);
+});
+  
+
